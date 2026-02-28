ca7c42728a8f2fe725644bf38f2396d928e56871
PKRampai1 <keoabetswe0502@gmail.com>
2026-02-28 22:28:42 +0200

    fix python version

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ async def webhook(request: Request):
     data = await request.body()
     s2 = re.sub(r'[^a-zA-Z0-9]', '', word)
     word = data.decode("utf-8")
-    a = list(word)
+    a = list(s2)
     a.sort()
-    return {"word": "".s2}
+    return {"word": "\"".join(a)}
