afc0df8270b581b3e83033dec7bc3aca0c0e6398
PKRampai1 <keoabetswe0502@gmail.com>
2026-02-28 20:21:50 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.post("/words")
+def create_item(word: str):
+    a = list(word)
+    a.sort()
+
+    return a
